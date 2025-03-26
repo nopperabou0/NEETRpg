3bdc90bfa424eaa7ba46a66c601076baf2557fd6
maherfauzi <azirariza9@gmail.com>
2025-03-26 21:46:47 +0700

    [add] ball falling

--- Game1.cs
+++ Game1.cs
@@ -9,9 +9,13 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
+    Texture2D ballTexture;
+    Vector2 ballPosition;
+    float ballVelocity;
+
     public Game1()
     {
-        _graphics = new GraphicsDeviceManager(this);
+        _graphics = new GraphicsDeviceManager(this);  
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
     }
@@ -19,6 +23,9 @@ public class Game1 : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
+        ballPosition = new Vector2(_graphics.PreferredBackBufferWidth/2,_graphics.PreferredBackBufferHeight/2);
+        ballVelocity = 10f;
+
 
         base.Initialize();
     }
@@ -26,7 +33,7 @@ public class Game1 : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+        ballTexture = Content.Load<Texture2D>("ball");
         // TODO: use this.Content to load your game content here
     }
 
@@ -35,17 +42,21 @@ public class Game1 : Game
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
-        // TODO: Add your update logic here
+        ballPosition.Y += ballVelocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+        GraphicsDevice.Clear(Color.White);
+        _spriteBatch.Begin();
+        
 
         // TODO: Add your drawing code here
-
+        _spriteBatch.Draw(ballTexture, ballPosition,Color.White);
+   
+        _spriteBatch.End();     
         base.Draw(gameTime);
     }
 }
